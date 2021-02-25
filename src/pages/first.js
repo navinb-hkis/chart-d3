6731a36e0abfcdb2ec9ef055e34bafa80ad16a87
navinb-hkis <navinb.hkis@gmail.com>
2021-02-25 12:51:18 +0530

    origin changes third

--- src/pages/first.js
+++ src/pages/first.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function First() {
-  return <div>This is first first pages.</div>;
+  return <div>This is first first bar chart pages.</div>;
 }
 
 export default First;
--- src/pages/second.js
+++ src/pages/second.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function First() {
-  return <div>This is Second origin page.</div>;
+  return <div>This is Second chart origin page.</div>;
 }
 
 export default First;
