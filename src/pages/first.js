5b5aeb716238164b5cfde64d5ece7bbc77e857ec
navinb-hkis <navinb.hkis@gmail.com>
2021-02-25 12:39:42 +0530

    second change

--- src/pages/first.js
+++ src/pages/first.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function First() {
-  return <div>This is develop develop pages.</div>;
+  return <div>This is first d3-chart2 pages.</div>;
 }
 
 export default First;
